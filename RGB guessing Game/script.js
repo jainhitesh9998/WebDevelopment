546347af244c2347b7ab3b67c879876521670657
Hitesh C <jainhitesh9998@gmail.com>
2017-06-15 22:46:03 +0530

    Added Play Again Option

--- RGB guessing Game/script.js
+++ RGB guessing Game/script.js
@@ -1,16 +1,29 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(255, 0, 255)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 0, 255)",
-	"rgb(0, 255, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	//generate all new colors
+	colors = generateRandomColors(6);
+	//new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match the picked color
+	colorDisplay.textContent = pickedColor;
+	// change colors of the square
+	for(var i = 0; i< squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+
+	resetButton.textContent = "New colors";
+	//default color
+	h1.style.backgroundColor = "#232323";
+
+	}
+});
 colorDisplay.textContent =  pickedColor;
 for(var i = 0; i < squares.length; i++){
 	//add initial colors to square
@@ -24,6 +37,8 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor){
 			changeColor(clickedColor);
 			messageDisplay.textContent = "Correct";	
+			resetButton.textContent = "Play Again?";
+			h1.style.backgroundColor = clickedColor;
 		}
 		else{
 			this.style.backgroundColor = "#232323";
@@ -43,4 +58,23 @@ function changeColor(color){
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = [];
+	for (var i = 0;i < num; i++){
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	//add num random colors to array
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red green blue between 0 -255"
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
