889d0cfd3767ab04486eb5e0db020ebb6026ad8d
Hitesh C <jainhitesh9998@gmail.com>
2017-06-15 23:54:03 +0530

    Added easy and hard mode

--- RGB guessing Game/script.js
+++ RGB guessing Game/script.js
@@ -1,15 +1,50 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function(){
+	numSquares = 3;
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		}else{
+			squares[i].style.display = "none";
+		}
+	}
+});
+hardBtn.addEventListener("click", function()
+{
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+	
+			squares[i].style.backgroundColor = colors[i];
+	
+			squares[i].style.display = "block";
+	
+	}
+});
 
 resetButton.addEventListener("click", function(){
 	//generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	//new random color from array
 	pickedColor = pickColor();
 	//change colorDisplay to match the picked color
@@ -20,7 +55,7 @@ resetButton.addEventListener("click", function(){
 
 	resetButton.textContent = "New colors";
 	//default color
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 
 	}
 });
