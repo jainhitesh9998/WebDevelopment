672a9e72d251a56f159641e165a925ab0b627a6d
Hitesh C <jainhitesh9998@gmail.com>
2017-06-22 23:06:06 +0530

    ToDo list project completed

---
+++ ToDoList/assets/js/script.js
@@ -0,0 +1,22 @@
+// check off specific todos by clicking
+
+$("ul").on("click", "li" ,function(){
+	$(this).toggleClass("completed");
+});
+//click on x to delete todos
+$("ul").on("click","span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+		//get the todo text
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> "+todoText+" </li>");
+	}
+});$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
