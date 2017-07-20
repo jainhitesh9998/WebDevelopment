6dfb381010108321ec563647eed39a9339797d42
Hitesh C <jainhitesh9998@gmail.com>
2017-07-20 18:12:04 +0000

    basic Express

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -1,1 +1,21 @@
-console.log("firstExpressApp");
+var express = require("express");
+var app = express();
+
+// "/" Hi
+app.get("/", function(req, res){
+   res.send("Hi There"); 
+});
+// "/bye" goodbye
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+// "/dog" meow
+app.get("/dog", function(req, res){
+    console.log("someone made a request to /dog")
+   res.send("meow"); 
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server Has Started");
+});
