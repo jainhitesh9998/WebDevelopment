97c8edcfd28a5512a8debb3e5d4d92637409197a
Hitesh C <jainhitesh9998@gmail.com>
2017-07-25 18:28:31 +0000

    added * and dynamic routes

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -16,6 +16,17 @@ app.get("/dog", function(req, res){
    res.send("meow"); 
 });
 
+app.get("/r/:subRedditName", function(req, res){
+    //console.log(req);
+    var subreddit = "WELCOME TO THE " + req.params.subRedditName + " SUBREDDIT";
+    subreddit = subreddit.toUpperCase();
+    res.send(subreddit);   
+});
+
+app.get("*", function(req, res) {
+   res.send("You are a star visitor");
+   console.log("got and invalid get request");
+});
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Server Has Started");
 });
