6293bf5b1742f91825cac7d42b797342d733766b
Hitesh C <jainhitesh9998@gmail.com>
2017-09-07 16:12:24 +0000

    conditionals added

--- MoreExpress/EJSdemo/app.js
+++ MoreExpress/EJSdemo/app.js
@@ -1,16 +1,28 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+//.ejs is optional now
+
 app.get("/", function(req, res){
-    res.render("home.ejs")
+    res.render("home")
 }); 
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
     //res.send("you fell in love with : "+ thing);
     //res.render("love.ejs", {thing}); or
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
+app.get("/posts", function(req, res) {
+    var posts = [
+        {title: "post1", author: "susie"}, 
+        {title: "Jon Snow", author: "GRRM"}, 
+        {title: "GOT", author: "ME"}, 
+        ];
+        res.render("posts.ejs", {posts: posts});
+})
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening");
 });
