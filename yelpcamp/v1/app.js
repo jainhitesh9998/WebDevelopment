f4e2c30a8da590822b267d3159c297529ef5231e
Hitesh C <jainhitesh9998@gmail.com>
2017-09-20 17:40:38 +0000

    v1 in progress

---
+++ yelpcamp/v1/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+          {name: "salmon creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"}, 
+          {name: "Granite Hill", image: "https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg"}, 
+          {name: "Old Trafford", image: "https://farm5.staticflickr.com/4007/4489630578_73c0948a76.jpg"}, 
+          {name: "Allianz Arena", image: "https://farm4.staticflickr.com/3241/2925011226_c150c7e78b.jpg"} 
+    ];
+    
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(req, res){
+    console.log("The YelpCamp server started");
+});
