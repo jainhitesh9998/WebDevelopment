24d3aec2f3243a1f6a405d90cf5a1d732e4dfc8b
Hitesh C <jainhitesh9998@gmail.com>
2017-09-27 17:53:04 +0000

    MongoDB sample file

---
+++ databases/cats.js
@@ -0,0 +1,54 @@
+var mongoose = require("mongoose");
+
+//connect
+mongoose.connect("mongodb://localhost/cat_app", {useMongoClient: true});
+
+//adding a new cat to DB
+var catSchema = new mongoose.Schema({
+   name: String,
+   age: Number,
+   temperament: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+// var george = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7,
+//     temperament: "Evil"
+// });
+
+// george.save(function(err, cat){
+//     if(err){
+//         console.log("Something went wrong");
+//     }else{
+//         console.log("We Saved a cat to DB");
+//         console.log(cat);
+//     }
+// });
+
+Cat.create({
+   name: "Snow White",
+   age: 15,
+   temperament: "Bland"
+}, function(err, cat){
+    if(err){
+        console.log("Snap!!!");
+    }else{
+        console.log(cat);
+    }
+});
+//retrive all cats from DB
+
+Cat.find({name: "Mrs. Norris"}, function(err, cats){
+   if(err){
+       console.log("Oh No error");
+       console.log(err);
+   } else{
+       console.log("All The cats");
+       console.log(cats);
+   }
+});
+
+
+
