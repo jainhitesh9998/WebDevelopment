a45d2c06c49f7b45f1ffcd9a95e1058ee1c33a4a
Hitesh C <jainhitesh9998@gmail.com>
2017-10-08 17:52:29 +0000

    added pther libraries

--- RESTful_routing/restfullblogapp/app.js
+++ RESTful_routing/restfullblogapp/app.js
@@ -3,6 +3,12 @@ var bodyParser =     require("body-parser"),
     express    =     require("express"),
     app        =     express();
     
+    
+mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient: true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+    
 //title
 //image
 //body
