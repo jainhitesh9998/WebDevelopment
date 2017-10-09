e1866042ec67332bf17e900e93a66a3da08c6980
Hitesh C <jainhitesh9998@gmail.com>
2017-10-09 16:55:51 +0000

    removed dummy data

--- RESTful_routing/restfullblogapp/app.js
+++ RESTful_routing/restfullblogapp/app.js
@@ -3,14 +3,31 @@ var bodyParser =     require("body-parser"),
     express    =     require("express"),
     app        =     express();
     
-    
-mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient: true});
+//App Config
+mongoose.connect("mongodb://localhost/restful_blog_app",{useMongoClient: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
     
-//title
-//image
-//body
-//date
+//Mongoose  Model Config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date, 
+        default: Date.now
+    }
+});
+
+var blog = mongoose.model("Blog", blogSchema);
+
+
+//Routes
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Server is Up"); 
+});
     
