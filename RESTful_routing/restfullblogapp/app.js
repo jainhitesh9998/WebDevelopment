6a416a9fcb3fd4979b127e9cbdc0eb37cc2bc521
Hitesh C <jainhitesh9998@gmail.com>
2017-10-19 15:39:11 +0000

    added new and create routes

--- RESTful_routing/restfullblogapp/app.js
+++ RESTful_routing/restfullblogapp/app.js
@@ -29,6 +29,7 @@ app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
+//INDEX Route
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -39,6 +40,24 @@ app.get("/blogs", function(req, res){
     })
 });
 
+//NEW Route
+app.get("/blogs/new",function(req, res) {
+   res.render("new"); 
+});
+
+//CREATE Route
+app.post("/blogs", function(req, res){
+   //Create Blog
+   Blog.create(req.body.blog, function(err, newBlog){
+       if(err){
+           res.render("new");
+       }else{
+           //redirect to the index
+           res.redirect("/blogs");
+       }
+   });
+   
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(req, res){
