55de686968668181a0a7edd0a46145a296006829
Hitesh C <jainhitesh9998@gmail.com>
2017-11-11 13:01:25 +0000

    completed the blog app

--- RESTful_routing/restfullblogapp/app.js
+++ RESTful_routing/restfullblogapp/app.js
@@ -1,13 +1,18 @@
-var bodyParser =     require("body-parser"),
-    mongoose   =     require("mongoose"),
-    express    =     require("express"),
-    app        =     express();
+var bodyParser       =     require("body-parser"),
+    methodOverride   = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
+    mongoose         =     require("mongoose"),
+    express          =     require("express"),
+    app              =     express();
     
 //App Config
 mongoose.connect("mongodb://localhost/restful_blog_app",{useMongoClient: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
     
 //Mongoose  Model Config
 var blogSchema = new mongoose.Schema({
@@ -48,6 +53,9 @@ app.get("/blogs/new",function(req, res) {
 //CREATE Route
 app.post("/blogs", function(req, res){
    //Create Blog
+   //console.log(req.body.blog.body);
+   req.body.blog.body = req.sanitize(req.body.blog.body);
+   //console.log(req.body.blog.body);
    Blog.create(req.body.blog, function(err, newBlog){
        if(err){
            res.render("new");
@@ -84,8 +92,28 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 //update route
 app.put("/blogs/:id", function(req, res){
-   res.send("UPDATE ROUTE!!"); 
+   req.body.blog.body = req.sanitize(req.body.blog.body);
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      } else{
+          res.redirect("/blogs/"+ req.params.id);
+      }
+   });
 });
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    //res.send("Destroy Route");
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            //res.send("notdeleted")
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs");
+        }
+    });
+})
 
 app.listen(process.env.PORT, process.env.IP, function(req, res){
    console.log("Server is Up"); 
