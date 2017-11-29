adb042a112efdfb4a6ed9fd35063c5e5f3ad2c50
Hitesh C <jainhitesh9998@gmail.com>
2017-11-29 17:55:57 +0000

    refactoring

--- Associations/references.js
+++ Associations/references.js
@@ -15,9 +15,34 @@ var Post = mongoose.model("Post", postSchema);
 
 var userSchema = new mongoose.Schema({
     email: String,
-    name : String,
-    posts: [postSchema]
+    name: String,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId ,
+            ref: "Post"
+        }
+        ]
 });
 
 var User = mongoose.model("User", userSchema);
+
+{
+    name: "asfassdfs",
+    posts: [
+            {title: "asdasdads", content: "asassfksdfsjk"},
+            {title: "asdasdads", content: "asassfksdfsjk"},
+            {title: "asdasdads", content: "asassfksdfsjk"},
+            {title: "asdasdads", content: "asassfksdfsjk"},
+            {title: "asdasdads", content: "asassfksdfsjk"}
+        ]
+};
+
+{
+    name: "asfassdfs",
+    posts: [
+            1312s35d45s4sd5,
+            d4as65das6d4542a1,
+            askadlsdj6w5asda5
+        ]
+};
 
